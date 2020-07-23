ac01666eb9049812c756ffae0af29bf5d4a945c2
Nicolay <59583646+pvpender@users.noreply.github.com>
2020-07-23 20:30:15 +0300

    Add files via upload

--- k_bot.py
+++ k_bot.py
@@ -33,14 +33,14 @@ async def ban(msg: types.message):
     except :
         await msg.answer('Ответьте на сообщение пользователя, которого хотите забанить')
 
-@dp.message_handler(is_chat_admin = False ,commands=['ban'])
-async def ban(msg: types.message):
+'''@dp.message_handler(is_chat_admin = False ,commands=['ban'])
+async def ban1(msg: types.message):
     a = time.time()
     a = a + 30
     await bot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, until_date=a,
                                    can_send_messages=False, can_send_media_messages=False,
                                    can_send_other_messages=False)
-    await msg.answer(f"Пользователь @{msg.from_user.username} замучен на 30 секунд! Причина: Тебе не давали права пользоваться ботом!")
+    await msg.answer(f"Пользователь @{msg.from_user.username} замучен на 30 секунд! Причина: Тебе не давали права пользоваться ботом!")'''
 
 @dp.message_handler(is_chat_admin = True,commands=['unban'])
 async def unban(msg: types.message):
@@ -49,14 +49,14 @@ async def unban(msg: types.message):
     except:
         await msg.answer('Ответьте на сообщение пользователя, которого хотите разбанить')
 
-@dp.message_handler(is_chat_admin = False ,commands=['unban'])
-async def unban(msg: types.message):
+'''@dp.message_handler(is_chat_admin = False ,commands=['unban'])
+async def unban1(msg: types.message):
     a = time.time()
     a = a + 30
     await bot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, until_date=a,
                                    can_send_messages=False, can_send_media_messages=False,
                                    can_send_other_messages=False)
-    await msg.answer(f"Пользователь @{msg.from_user.username} замучен на 30 секунд! Причина: Тебе не давали права пользоваться ботом!")
+    await msg.answer(f"Пользователь @{msg.from_user.username} замучен на 30 секунд! Причина: Тебе не давали права пользоваться ботом!")'''
 
 @dp.message_handler(is_chat_admin = True,commands=['mute'])
 async def mute(msg: types.message):
@@ -80,14 +80,14 @@ async def mute(msg: types.message):
    except:
        await msg.answer('Ответьте на сообщение пользователя, которого хотите замутить')
 
-@dp.message_handler(is_chat_admin = False ,commands=['mute'])
-async def ban(msg: types.message):
+'''@dp.message_handler(is_chat_admin = False ,commands=['mute'])
+async def mute1(msg: types.message):
     a = time.time()
     a = a + 30
     await bot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, until_date=a,
                                    can_send_messages=False, can_send_media_messages=False,
                                    can_send_other_messages=False)
-    await msg.answer(f"Пользователь @{msg.from_user.username} замучен на 30 секунд! Причина: Тебе не давали права пользоваться ботом!")
+    await msg.answer(f"Пользователь @{msg.from_user.username} замучен на 30 секунд! Причина: Тебе не давали права пользоваться ботом!")'''
 
 @dp.message_handler(is_chat_admin = True,commands=['unmute'])
 async def unmute(msg: types.message):
@@ -99,14 +99,14 @@ async def unmute(msg: types.message):
         await msg.answer('Ответьте на сообщение пользователя, которого хотите размутить')
 
 
-@dp.message_handler(is_chat_admin = False ,commands=['unmute'])
-async def ban(msg: types.message):
+'''@dp.message_handler(is_chat_admin = True ,commands=['unmute'])
+async def unmute1(msg: types.message):
     a = time.time()
     a = a + 30
     await bot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, until_date=a,
                                    can_send_messages=False, can_send_media_messages=False,
                                    can_send_other_messages=False)
-    await msg.answer(f"Пользователь @{msg.from_user.username} замучен на 30 секунд! Причина: Тебе не давали права пользоваться ботом!")
+    await msg.answer(f"Пользователь @{msg.from_user.username} замучен на 30 секунд! Причина: Тебе не давали права пользоваться ботом!")'''
 
 @dp.message_handler(commands=['help'])
 async def help(msg: types.message):
