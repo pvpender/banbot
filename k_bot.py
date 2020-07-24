24b22ce7de9d56bf326e6f37003b0abf6c6a24ce
Nicolay <59583646+pvpender@users.noreply.github.com>
2020-07-24 16:10:15 +0300

    Add files via upload

--- k_bot.py
+++ k_bot.py
@@ -1,7 +1,9 @@
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters import BoundFilter
 import logging
 import time
+
 
 logging.basicConfig(level=logging.INFO)
 storage = MemoryStorage()
@@ -9,12 +11,29 @@ TOKEN = '1264365351:AAH38ocd1resSc_MyQyHLTNjlFUw3hbp4iI'
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot, storage=storage)
 
+class CheckFilter(BoundFilter):
+    key = 'is_admin'
+    def __init__(self, is_admin):
+        self.is_admin = is_admin
+    async def check(self, message: types.Message):
+       member = await bot.get_chat_member(message.chat.id, message.from_user.id)
+       return member.can_restrict_members == self.is_admin
+dp.filters_factory.bind(CheckFilter)
+
+class CheckFilter(BoundFilter):
+    key = 'is_creator'
+    def __init__(self, is_creator):
+        self.is_creator = is_creator
+    async def check(self, message: types.Message):
+       member = await bot.get_chat_member(message.chat.id, message.from_user.id)
+       return member.status == self.is_creator
+dp.filters_factory.bind(CheckFilter)
 
 @dp.message_handler(commands= ['start'])
 async def st(msg: types.message):
     await msg.answer('Я готов к работе!')
 
-@dp.message_handler(is_chat_admin = True,commands=['ban'])
+@dp.message_handler(is_admin = True,commands=['ban'])
 async def ban(msg: types.message):
     try:
         h = msg.text
@@ -22,7 +41,26 @@ async def ban(msg: types.message):
         try:
             b = h[6:len(h)]
             a = a + int(b)
-            if int(b)<32 or int(b) >1314000:
+            if int(b)<30 or int(b) >1314000:
+                await msg.answer('Слишком маленький или слишком большой промежуток времени!')
+            else:
+                await bot.kick_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, until_date=a)
+                await msg.answer(f"Пользователь @{msg.reply_to_message.from_user.username} забанен в группе на {b} секунд!")
+        except:
+             await bot.kick_chat_member(msg.chat.id, msg.reply_to_message.from_user.id)
+             await msg.answer(f"Пользователь @{msg.reply_to_message.from_user.username} забанен в группе навсегда!")
+    except :
+        await msg.answer('Ответьте на сообщение пользователя, которого хотите забанить')
+
+@dp.message_handler(is_creator = 'creator',commands=['ban'])
+async def ban(msg: types.message):
+    try:
+        h = msg.text
+        a = time.time()
+        try:
+            b = h[6:len(h)]
+            a = a + int(b)
+            if int(b)<30 or int(b) >1314000:
                 await msg.answer('Слишком маленький или слишком большой промежуток времени!')
             else:
                 await bot.kick_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, until_date=a)
@@ -36,13 +74,20 @@ async def ban(msg: types.message):
 '''@dp.message_handler(is_chat_admin = False ,commands=['ban'])
 async def ban1(msg: types.message):
     a = time.time()
-    a = a + 31
+    a = a + 30
     await bot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, until_date=a,
                                    can_send_messages=False, can_send_media_messages=False,
                                    can_send_other_messages=False)
     await msg.answer(f"Пользователь @{msg.from_user.username} замучен на 30 секунд! Причина: Тебе не давали права пользоваться ботом!")'''
 
-@dp.message_handler(is_chat_admin = True,commands=['unban'])
+@dp.message_handler(is_admin = True,commands=['unban'])
+async def unban(msg: types.message):
+    try:
+        await bot.unban_chat_member(msg.chat.id, msg.reply_to_message.from_user.id)
+    except:
+        await msg.answer('Ответьте на сообщение пользователя, которого хотите разбанить')
+
+@dp.message_handler(is_creator='creator',commands=['unban'])
 async def unban(msg: types.message):
     try:
         await bot.unban_chat_member(msg.chat.id, msg.reply_to_message.from_user.id)
@@ -52,13 +97,13 @@ async def unban(msg: types.message):
 '''@dp.message_handler(is_chat_admin = False ,commands=['unban'])
 async def unban1(msg: types.message):
     a = time.time()
-    a = a + 31
+    a = a + 30
     await bot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, until_date=a,
                                    can_send_messages=False, can_send_media_messages=False,
                                    can_send_other_messages=False)
     await msg.answer(f"Пользователь @{msg.from_user.username} замучен на 30 секунд! Причина: Тебе не давали права пользоваться ботом!")'''
 
-@dp.message_handler(is_chat_admin = True,commands=['mute'])
+@dp.message_handler(is_admin = True,commands=['mute'])
 async def mute(msg: types.message):
    try:
         h = msg.text
@@ -66,7 +111,29 @@ async def mute(msg: types.message):
         try:
             b = h[6:len(h)]
             a = a + int(b)
-            if int(b) < 32 or int(b) > 1314000:
+            if int(b) < 30 or int(b) > 1314000:
+                await msg.answer('Слишком маленький или слишком большой промежуток времени!')
+            else:
+                  await bot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, until_date=a, can_send_messages=False, can_send_media_messages=False,
+                                                   can_send_other_messages=False)
+                  await msg.answer (f"Пользователю @{msg.reply_to_message.from_user.username} запрещено писать в группе на {int(b)} секунд!")
+        except:
+            await bot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, can_send_messages=False, can_send_media_messages=False,
+                                           can_send_other_messages=False)
+            await msg.answer(
+                f"Пользователю @{msg.reply_to_message.from_user.username} запрещено писать в группе навсегда!")
+   except:
+       await msg.answer('Ответьте на сообщение пользователя, которого хотите замутить')
+
+@dp.message_handler(is_creator='creator',commands=['mute'])
+async def mute(msg: types.message):
+   try:
+        h = msg.text
+        a = time.time()
+        try:
+            b = h[6:len(h)]
+            a = a + int(b)
+            if int(b) < 30 or int(b) > 1314000:
                 await msg.answer('Слишком маленький или слишком большой промежуток времени!')
             else:
                   await bot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, until_date=a, can_send_messages=False, can_send_media_messages=False,
@@ -89,15 +156,23 @@ async def mute1(msg: types.message):
                                    can_send_other_messages=False)
     await msg.answer(f"Пользователь @{msg.from_user.username} замучен на 30 секунд! Причина: Тебе не давали права пользоваться ботом!")'''
 
-@dp.message_handler(is_chat_admin = True,commands=['unmute'])
+@dp.message_handler(is_admin = True,commands=['unmute'])
 async def unmute(msg: types.message):
     try:
          await bot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, can_send_messages=True, can_send_media_messages=True,
-                                        can_send_other_messages=True,can_add_web_page_previews=True)
+                                        can_send_other_messages=True)
          await msg.answer('Пользователь размучен!')
     except:
         await msg.answer('Ответьте на сообщение пользователя, которого хотите размутить')
 
+@dp.message_handler(is_creator='creator',commands=['unmute'])
+async def unmute(msg: types.message):
+    try:
+         await bot.restrict_chat_member(msg.chat.id, msg.reply_to_message.from_user.id, can_send_messages=True, can_send_media_messages=True,
+                                        can_send_other_messages=True)
+         await msg.answer('Пользователь размучен!')
+    except:
+        await msg.answer('Ответьте на сообщение пользователя, которого хотите размутить')
 
 '''@dp.message_handler(is_chat_admin = True ,commands=['unmute'])
 async def unmute1(msg: types.message):
