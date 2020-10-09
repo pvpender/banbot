20a6db982da1ff35c7ecd568ce06206099a5d334
Nicolay <59583646+pvpender@users.noreply.github.com>
2020-10-09 18:27:06 +0300

    upe

--- k_bot.py
+++ k_bot.py
@@ -324,12 +324,12 @@ async def send_report(msg: types.message):
 @dp.callback_query_handler(lambda m: m.data == "d")
 async def d(m):
     try:
-        chat_id = m.message.entities[0].ulr
-        chat_id = chat_id.split("/")
-        chat_id = chat_id[2]
+        #chat_id = m.message.entities[0].ulr
+        #chat_id = chat_id.split("/")
+        #chat_id = chat_id[2]
         #chat_id1 = int(chat_id[8:])
-        message_id = m.message.entities[3].ulr
-        message_id = message_id.split("/")
+        #message_id = m.message.entities[3].ulr
+       # message_id = message_id.split("/")
         #message_id1 = int(message_id[4])
         #await bot.delete_message(chat_id1, message_id1)
         await m.answer("Готово! Я удалил сообщение!")
