f23912d324e1ea267bee033c8135576aeb427b9e
Nicolay <59583646+pvpender@users.noreply.github.com>
2020-10-09 16:59:06 +0300

    upe

--- k_bot.py
+++ k_bot.py
@@ -296,11 +296,12 @@ async def send_report(msg: types.message):
         admins = await bot.get_chat_administrators(msg.chat.id)
         chat_link = f"tg://chat?id={msg.chat.id}"
         chat_high_link = hlink(f"{msg.chat.title}", chat_link)
-        await msg.answer("Я отправил жалобу администраторам!")
+
         user1_link = f"tg://user?id={msg.reply_to_message.from_user.id}"
         user1 = hlink(f"{msg.reply_to_message.from_user.first_name}", user1_link)
         user2_link = f"tg://user?id={msg.from_user.id}"
         user2 = hlink(f"{msg.from_user.firstname}", user2_link)
+        await msg.answer("Я отправил жалобу администраторам!")
         for a in admins:
             if (a.can_delete_messages is True or a.status == "creator") and a.user.is_bot is False:
                 try:
